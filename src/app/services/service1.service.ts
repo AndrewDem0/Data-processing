e80e5a595cb6a21127fb9b1df0d7425f3130b5e3
AndrewDem0 <144722259+AndrewDem0@users.noreply.github.com>
2024-04-30 15:05:26 +0300

    commit try to fix

--- src/app/services/service1.service.ts
+++ src/app/services/service1.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable, ObservedValueOf } from 'rxjs';
 import { Entity } from '../interfaces/entity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Service1Service {
-  url: string = 'http://localhost:8080/lab1/Serlvet1'
-  constructor(private http: HttpClient) { }
+url:string="http://localhost:8080/lab1/Serlvet1"
 
-  getEntities(): Observable<Entity[]> {
+  constructor(private http:HttpClient) { }
 
+  getEntities():Observable<Entity[]>{
     return this.http.get<Entity[]>(this.url);
   }
 }
